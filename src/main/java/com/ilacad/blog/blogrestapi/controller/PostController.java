ff42528ccb911f766813de81618d06751a8f40a4
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-05 01:32:35 +0800

    Inject PostService in PostController

--- src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
@@ -1,9 +1,16 @@
 package com.ilacad.blog.blogrestapi.controller;
 
+import com.ilacad.blog.blogrestapi.service.PostService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 }
