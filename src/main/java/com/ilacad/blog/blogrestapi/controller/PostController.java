6d68ba87ce4fff56e735eff571634d83feebe0e9
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-05 01:31:46 +0800

    Initialize PostController

---
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
@@ -0,0 +1,9 @@
+package com.ilacad.blog.blogrestapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+}
