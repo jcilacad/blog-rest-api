8ba7b90f7682d29163f333bc1dadb7fbfa291951
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-05 01:34:40 +0800

    Add a createPost method in PostController for adding new post

--- src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
@@ -1,6 +1,11 @@
 package com.ilacad.blog.blogrestapi.controller;
 
+import com.ilacad.blog.blogrestapi.payload.PostDto;
 import com.ilacad.blog.blogrestapi.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,10 @@ public class PostController {
 
     public PostController(PostService postService) {
         this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost (@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 }
