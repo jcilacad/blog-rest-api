f36f4cd13627c0b06a138443159ca0ec19e7900c
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-05 23:48:57 +0800

    Add signature method for getAllPosts()

--- src/main/java/com/ilacad/blog/blogrestapi/service/PostService.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/PostService.java
@@ -2,6 +2,10 @@ package com.ilacad.blog.blogrestapi.service;
 
 import com.ilacad.blog.blogrestapi.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
 }
