40466e72250f39b7f12442a91ec32310e2f17789
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-05 23:51:37 +0800

    Create a method to map entity and dto

--- src/main/java/com/ilacad/blog/blogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/impl/PostServiceImpl.java
@@ -6,6 +6,8 @@ import com.ilacad.blog.blogrestapi.repository.PostRepository;
 import com.ilacad.blog.blogrestapi.service.PostService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -18,7 +20,34 @@ public class PostServiceImpl implements PostService {
     @Override
     public PostDto createPost(PostDto postDto) {
 
-        // Convert the DTO to entity
+        // Save the post to database
+        Post newPost = postRepository.save(post);
+
+
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        return null;
+    }
+
+    // Convert entity to dto
+    private PostDto mapToDto(Post post) {
+
+        PostDto postDto = new PostDto(
+                post.getId(),
+                post.getTitle(),
+                post.getDescription(),
+                post.getContent()
+        );
+
+        return postDto;
+    }
+
+    // Convert the DTO to entity
+    private Post mapToEntity(PostDto postDto) {
+
         Post post = new Post(
                 postDto.getId(),
                 postDto.getTitle(),
@@ -26,17 +55,6 @@ public class PostServiceImpl implements PostService {
                 postDto.getContent()
         );
 
-        // Save the post to database
-        Post newPost = postRepository.save(post);
-
-        // Convert entity to DTO
-        PostDto postResponse = new PostDto(
-                newPost.getId(),
-                newPost.getTitle(),
-                newPost.getDescription(),
-                newPost.getContent()
-        );
-
-        return postResponse;
+        return post;
     }
 }
