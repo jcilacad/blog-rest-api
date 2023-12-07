9c825b6df3d6a9cb55a4c25670ad4ef182dd3b8f
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-07 10:45:25 +0800

    Add signature and implementation method for deleting a post

--- src/main/java/com/ilacad/blog/blogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/impl/PostServiceImpl.java
@@ -90,4 +90,11 @@ public class PostServiceImpl implements PostService {
 
         return mapToDto(updatedPost);
     }
+
+    @Override
+    public void deletePostById(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+        postRepository.delete(post);
+    }
 }
