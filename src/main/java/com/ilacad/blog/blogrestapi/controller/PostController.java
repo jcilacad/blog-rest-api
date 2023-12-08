e96ab9143bf5eafaa390912e662f58fdb630f4b4
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-08 15:21:10 +0800

    Implement pagination in controller layer

--- src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
@@ -24,8 +24,10 @@ public class PostController {
     }
 
     @GetMapping
-    public List<PostDto> getAllPosts() {
-        return postService.getAllPosts();
+    public List<PostDto> getAllPosts(@RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+                                     @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize) {
+
+        return postService.getAllPosts(pageNo, pageSize);
     }
 
     @GetMapping("{id}")
