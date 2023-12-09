992d43f9ae66072b5f0954fcf1425011ab6b3ee5
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-09 16:07:52 +0800

    Implement the functionality of creating a new comment in CommentServiceImpl

--- src/main/java/com/ilacad/blog/blogrestapi/service/impl/CommentServiceImpl.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/impl/CommentServiceImpl.java
@@ -1,17 +1,41 @@
 package com.ilacad.blog.blogrestapi.service.impl;
 
 import com.ilacad.blog.blogrestapi.entity.Comment;
+import com.ilacad.blog.blogrestapi.entity.Post;
+import com.ilacad.blog.blogrestapi.exception.ResourceNotFoundException;
 import com.ilacad.blog.blogrestapi.payload.CommentDto;
+import com.ilacad.blog.blogrestapi.repository.CommentRepository;
+import com.ilacad.blog.blogrestapi.repository.PostRepository;
 import com.ilacad.blog.blogrestapi.service.CommentService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentServiceImpl implements CommentService {
 
+    private CommentRepository commentRepository;
+    private PostRepository postRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
     @Override
     public CommentDto createComment(Long postId, CommentDto commentDto) {
 
-        return null;
+        Comment comment = mapToEntity(commentDto);
+
+        // Retrieve post by id
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundException("Post", "id", postId));
+
+        // Set the comment post
+        comment.setPost(post);
+
+        // Save the new comment to database
+        Comment newComment = commentRepository.save(comment);
+        
+        return mapToDto(newComment);
     }
 
     private CommentDto mapToDto(Comment comment) {
@@ -34,7 +58,7 @@ public class CommentServiceImpl implements CommentService {
                 commentDto.getEmail(),
                 commentDto.getBody()
         );
-        
+
         return comment;
 
     }
