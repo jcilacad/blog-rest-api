f59aea732ecf40effca4317addf3179a483707d0
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-09 15:49:51 +0800

    Initialize CommentServiceImpl

---
+++ src/main/java/com/ilacad/blog/blogrestapi/service/impl/CommentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ilacad.blog.blogrestapi.service.impl;
+
+import com.ilacad.blog.blogrestapi.service.CommentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    
+}
