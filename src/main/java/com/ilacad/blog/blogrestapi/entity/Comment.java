51369cf480af19a0366632ffcddb4650cc9e057e
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-09 14:22:34 +0800

    Initialize comment entity

---
+++ src/main/java/com/ilacad/blog/blogrestapi/entity/Comment.java
@@ -0,0 +1,18 @@
+package com.ilacad.blog.blogrestapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    
+}
