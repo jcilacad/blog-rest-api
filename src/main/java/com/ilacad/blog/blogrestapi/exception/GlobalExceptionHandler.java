191565d161df9e582cd321513f694835129baa63
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-12 16:46:33 +0800

    Add specific exception handlers for ResourceNotFound and BlogApi

---
+++ src/main/java/com/ilacad/blog/blogrestapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.ilacad.blog.blogrestapi.exception;
+
+import com.ilacad.blog.blogrestapi.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException,
+                                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),
+                resourceNotFoundException.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BlogApiException.class)
+    public ResponseEntity<ErrorDetails> handleBlogApiException(BlogApiException blogApiException,
+                                                                   WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),
+                blogApiException.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+}
