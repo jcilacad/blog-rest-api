6873a159fd74a424ad95d536f8c9ee3ea64d40b1
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-13 10:56:10 +0800

    Add validation for fields in PostDto

--- src/main/java/com/ilacad/blog/blogrestapi/payload/PostDto.java
+++ src/main/java/com/ilacad/blog/blogrestapi/payload/PostDto.java
@@ -1,5 +1,7 @@
 package com.ilacad.blog.blogrestapi.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.Set;
@@ -11,8 +13,17 @@ import java.util.Set;
 public class PostDto {
 
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should be at least 2 characters")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+
+    @NotEmpty
     private String content;
+    
     private Set<CommentDto> comments;
 }
