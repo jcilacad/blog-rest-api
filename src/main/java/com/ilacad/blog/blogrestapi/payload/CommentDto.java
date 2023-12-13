f1010b1926f80f446c0f53a7d05c934a6dc42264
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-14 00:02:13 +0800

    Add bean validations in CommentDto

--- src/main/java/com/ilacad/blog/blogrestapi/payload/CommentDto.java
+++ src/main/java/com/ilacad/blog/blogrestapi/payload/CommentDto.java
@@ -1,5 +1,8 @@
 package com.ilacad.blog.blogrestapi.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,7 +12,15 @@ import lombok.*;
 public class CommentDto {
 
     private Long id;
+
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    @NotEmpty(message = "Body should not be null or empty")
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
     private String body;
 }
