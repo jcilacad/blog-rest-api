6b52a8d396f1f4083f32294a0d27779e325f16c6
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-13 14:42:10 +0800

    Add custom exception handler for MethodArgumentNotValid

--- src/main/java/com/ilacad/blog/blogrestapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ilacad/blog/blogrestapi/exception/GlobalExceptionHandler.java
@@ -3,11 +3,16 @@ package com.ilacad.blog.blogrestapi.exception;
 import com.ilacad.blog.blogrestapi.payload.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -27,6 +32,27 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(),
                 blogApiException.getMessage(),
                 webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails[]> handleMethodArgumentNotValidException (MethodArgumentNotValidException methodArgumentNotValidException,
+                                                                          WebRequest webRequest) {
+
+        Map<String, String> errors = new HashMap<>();
+        methodArgumentNotValidException.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        final ErrorDetails[] errorDetails = new ErrorDetails[1];
+
+        errors.forEach((key, value) -> {
+            errorDetails[0] = new ErrorDetails(new Date(),
+                    value, webRequest.getDescription(false));
+        });
+
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
