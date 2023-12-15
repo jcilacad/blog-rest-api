6205744485e358ed473fa9b2fc858697fcc62665
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-15 15:51:00 +0800

    Authenticate all GET http method

--- src/main/java/com/ilacad/blog/blogrestapi/config/SecurityConfig.java
+++ src/main/java/com/ilacad/blog/blogrestapi/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.ilacad.blog.blogrestapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,12 +15,25 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public static PasswordEncoder passwordEncoder () {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                            .anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
     }
 
     @Bean
@@ -38,16 +53,5 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(jc, jp);
-    }
-
-    @Bean
-    SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> {
-                    authorize.anyRequest().authenticated();
-                }).httpBasic(Customizer.withDefaults());
-
-        return httpSecurity.build();
     }
 }
