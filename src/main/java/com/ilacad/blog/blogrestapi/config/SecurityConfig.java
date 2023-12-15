a36b202d7fa22172bd0646be43ea3e2573108e76
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-15 15:38:45 +0800

    Add in memory user details in security config

--- src/main/java/com/ilacad/blog/blogrestapi/config/SecurityConfig.java
+++ src/main/java/com/ilacad/blog/blogrestapi/config/SecurityConfig.java
@@ -4,10 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+
+        UserDetails jc = User.builder()
+                .username("jc")
+                .password("jc")
+                .roles("ADMIN")
+                .build();
+
+
+        UserDetails jp = User.builder()
+                .username("jp")
+                .password("jp")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(jc, jp);
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
