c1ea79bdfd468ae94f7dcd0fd39567f92e5df50d
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-16 16:40:13 +0800

    Add rest api for login/signin

--- src/main/java/com/ilacad/blog/blogrestapi/controller/AuthController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.ilacad.blog.blogrestapi.controller;
 
+import com.ilacad.blog.blogrestapi.payload.LoginDto;
 import com.ilacad.blog.blogrestapi.service.AuthService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,11 @@ public class AuthController {
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
-    
+
+    @PostMapping(value = {"login", "signin"})
+    public ResponseEntity<String> login (@RequestBody LoginDto loginDto) {
+        String response = authService.login(loginDto);
+        return ResponseEntity.ok(response);
+    }
+
 }
