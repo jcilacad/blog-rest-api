6d92f2e36bbc1f639c43de29b1a2de3c1995da86
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-17 02:21:02 +0800

    Add signature method for registration of user in UserService

--- src/main/java/com/ilacad/blog/blogrestapi/service/AuthService.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.ilacad.blog.blogrestapi.service;
 
 import com.ilacad.blog.blogrestapi.payload.LoginDto;
+import com.ilacad.blog.blogrestapi.payload.RegisterDto;
 
 public interface AuthService {
     String login(LoginDto loginDto);
+    String register(RegisterDto registerDto);
 }
