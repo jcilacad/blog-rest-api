ffcd82e63f52e87a9226e1f0b86965d1e11a5883
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-17 02:35:48 +0800

    Add rest api for registration of user

--- src/main/java/com/ilacad/blog/blogrestapi/controller/AuthController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.ilacad.blog.blogrestapi.controller;
 
 import com.ilacad.blog.blogrestapi.payload.LoginDto;
+import com.ilacad.blog.blogrestapi.payload.RegisterDto;
 import com.ilacad.blog.blogrestapi.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +24,12 @@ public class AuthController {
     public ResponseEntity<String> login (@RequestBody LoginDto loginDto) {
         String response = authService.login(loginDto);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = {"register", "signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 }
