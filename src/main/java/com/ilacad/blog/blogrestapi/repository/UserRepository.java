f8623a8f72525b46f9500d32f914cc1e23acc00d
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-17 02:20:07 +0800

    Add signature methods in UserRepository to check if the user exixst by email and username

--- src/main/java/com/ilacad/blog/blogrestapi/repository/UserRepository.java
+++ src/main/java/com/ilacad/blog/blogrestapi/repository/UserRepository.java
@@ -8,4 +8,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmailOrUsername(String email, String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
 }
