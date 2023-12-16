a26804e18f208b1671a6e0f2fccf04b7f0300900
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-16 16:35:27 +0800

    Inject AuthService in AuthController

--- src/main/java/com/ilacad/blog/blogrestapi/controller/AuthController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.ilacad.blog.blogrestapi.controller;
 
+import com.ilacad.blog.blogrestapi.service.AuthService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
     
 }
