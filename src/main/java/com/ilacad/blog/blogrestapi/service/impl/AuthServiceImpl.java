430a71987416adf43bd38993135e531c86915007
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-17 02:23:13 +0800

    Implement register method in and inject necessary dependencies

--- src/main/java/com/ilacad/blog/blogrestapi/service/impl/AuthServiceImpl.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/impl/AuthServiceImpl.java
@@ -1,20 +1,33 @@
 package com.ilacad.blog.blogrestapi.service.impl;
 
 import com.ilacad.blog.blogrestapi.payload.LoginDto;
+import com.ilacad.blog.blogrestapi.payload.RegisterDto;
+import com.ilacad.blog.blogrestapi.repository.RoleRepository;
+import com.ilacad.blog.blogrestapi.repository.UserRepository;
 import com.ilacad.blog.blogrestapi.service.AuthService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+    public AuthServiceImpl(AuthenticationManager authenticationManager,
+                           UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -27,5 +40,10 @@ public class AuthServiceImpl implements AuthService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return "User logged in successfully.";
+    }
+
+    @Override
+    public String register(RegisterDto registerDto) {
+        return null;
     }
 }
