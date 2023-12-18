364e1b1abf84512bb23782d0f2cd26f6309779af
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-18 23:19:19 +0800

    Add a method to get token from request

--- src/main/java/com/ilacad/blog/blogrestapi/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ilacad/blog/blogrestapi/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -25,5 +26,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+
+        return null;
     }
 }
