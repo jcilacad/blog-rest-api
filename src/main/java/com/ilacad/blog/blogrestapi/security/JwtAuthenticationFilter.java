67aa6fda83af5eb01512cf1828f5d428f05c5a3f
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-18 23:29:48 +0800

    Implement the functionality of doFilterInternal method

--- src/main/java/com/ilacad/blog/blogrestapi/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ilacad/blog/blogrestapi/security/JwtAuthenticationFilter.java
@@ -4,7 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -24,7 +28,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        // Get the Jwt from the Http Request
+        String token = getTokenFromRequest(request);
+
+        // Validate Token
+        if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+
+            // Get username from token
+            String username = jwtTokenProvider.getUsername(token);
+
+            // Load the user associated with the token
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+
 
     }
 
