7bae4f1e5b8a9a1bb42f838f08907f70f81e69f8
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-20 03:10:28 +0800

    Make an admin to only have an access of adding cateogry rest api

--- src/main/java/com/ilacad/blog/blogrestapi/controller/CategoryController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/CategoryController.java
@@ -4,6 +4,8 @@ import com.ilacad.blog.blogrestapi.payload.CategoryDto;
 import com.ilacad.blog.blogrestapi.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,8 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CategoryDto> addCategory(CategoryDto categoryDto) {
         CategoryDto savedCategory = categoryService.addCategory(categoryDto);
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
