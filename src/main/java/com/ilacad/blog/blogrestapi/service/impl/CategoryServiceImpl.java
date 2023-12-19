89a81f79ecd34adcac6c8cb47a3836572762a4e2
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-20 02:55:11 +0800

    Implement the functionality of adding a category in implementation class

--- src/main/java/com/ilacad/blog/blogrestapi/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/impl/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ilacad.blog.blogrestapi.service.impl;
 
+import com.ilacad.blog.blogrestapi.entity.Category;
+import com.ilacad.blog.blogrestapi.mapper.CategoryMapper;
 import com.ilacad.blog.blogrestapi.payload.CategoryDto;
 import com.ilacad.blog.blogrestapi.repository.CategoryRepository;
 import com.ilacad.blog.blogrestapi.service.CategoryService;
@@ -8,8 +10,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
+    private CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @Override
     public CategoryDto addCategory(CategoryDto categoryDto) {
-        return null;
+
+        // Convert category dto to entity
+        Category category = CategoryMapper.INSTANCE.categoryDtoToCategory(categoryDto);
+
+        // Save the category in database
+        Category savedCategory = categoryRepository.save(category);
+
+        return CategoryMapper.INSTANCE.categoryToCategoryDto(savedCategory);
     }
 }
