5290f17e4674a4cde55087b8cf8d1c2b41dd2cda
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-20 02:45:33 +0800

    Add signature method for adding a category

--- src/main/java/com/ilacad/blog/blogrestapi/service/CategoryService.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/CategoryService.java
@@ -1,7 +1,10 @@
 package com.ilacad.blog.blogrestapi.service;
 
+import com.ilacad.blog.blogrestapi.payload.CategoryDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CategoryService {
+
+    CategoryDto addCategory(CategoryDto categoryDto);
 }
