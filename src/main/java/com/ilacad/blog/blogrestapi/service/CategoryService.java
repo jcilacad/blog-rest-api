7fc0f275d4991ac6124644b52d82143e96682010
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-20 02:47:11 +0800

    Add CategoryServiceImpl and implement its service interface

--- src/main/java/com/ilacad/blog/blogrestapi/service/CategoryService.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/CategoryService.java
@@ -1,9 +1,7 @@
 package com.ilacad.blog.blogrestapi.service;
 
 import com.ilacad.blog.blogrestapi.payload.CategoryDto;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface CategoryService {
 
     CategoryDto addCategory(CategoryDto categoryDto);
---
+++ src/main/java/com/ilacad/blog/blogrestapi/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ilacad.blog.blogrestapi.service.impl;
+
+import com.ilacad.blog.blogrestapi.payload.CategoryDto;
+import com.ilacad.blog.blogrestapi.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Override
+    public CategoryDto addCategory(CategoryDto categoryDto) {
+        return null;
+    }
+}
