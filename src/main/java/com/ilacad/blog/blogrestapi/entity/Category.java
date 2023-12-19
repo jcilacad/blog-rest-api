1cb203371b9f04a9bcf2cc092cb16fedca1c5028
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-20 01:55:46 +0800

    Add Category entity

---
+++ src/main/java/com/ilacad/blog/blogrestapi/entity/Category.java
@@ -0,0 +1,22 @@
+package com.ilacad.blog.blogrestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+}
