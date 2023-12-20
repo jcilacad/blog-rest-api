edd5c7c7f240ef8fe2de52e7e3e20d656ffa3f54
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-20 16:46:47 +0800

    Implement the CategoryService

--- src/main/java/com/ilacad/blog/blogrestapi/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ilacad.blog.blogrestapi.service.impl;
 
 import com.ilacad.blog.blogrestapi.entity.Category;
+import com.ilacad.blog.blogrestapi.exception.ResourceNotFoundException;
 import com.ilacad.blog.blogrestapi.mapper.CategoryMapper;
 import com.ilacad.blog.blogrestapi.payload.CategoryDto;
 import com.ilacad.blog.blogrestapi.repository.CategoryRepository;
 import com.ilacad.blog.blogrestapi.service.CategoryService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -21,5 +23,13 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = CategoryMapper.INSTANCE.categoryDtoToCategory(categoryDto);
         Category savedCategory = categoryRepository.save(category);
         return CategoryMapper.INSTANCE.categoryToCategoryDto(savedCategory);
+    }
+
+    @Override
+    public CategoryDto getCategory(Long categoryId) {
+        Category category = categoryRepository
+                .findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "id", categoryId));
+        return CategoryMapper.INSTANCE.categoryToCategoryDto(category);
     }
 }
