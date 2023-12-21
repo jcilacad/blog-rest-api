171fefea6bd67726d36a4bab99b8852c7b5bef28
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-21 23:20:38 +0800

    Add signature method of getCategories()

--- src/main/java/com/ilacad/blog/blogrestapi/service/CategoryService.java
+++ src/main/java/com/ilacad/blog/blogrestapi/service/CategoryService.java
@@ -2,9 +2,13 @@ package com.ilacad.blog.blogrestapi.service;
 
 import com.ilacad.blog.blogrestapi.payload.CategoryDto;
 
+import java.util.List;
+
 public interface CategoryService {
 
     CategoryDto addCategory(CategoryDto categoryDto);
 
     CategoryDto getCategory(Long categoryId);
+
+    List<CategoryDto> getCategories();
 }
