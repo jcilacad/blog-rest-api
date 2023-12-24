c4ff85ff92a9c247dbfa5288202d90fd23cf6f1f
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-24 20:19:43 +0800

    Add signature method to find post by category id

--- src/main/java/com/ilacad/blog/blogrestapi/repository/PostRepository.java
+++ src/main/java/com/ilacad/blog/blogrestapi/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.ilacad.blog.blogrestapi.repository;
 import com.ilacad.blog.blogrestapi.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByCategory_id(Long categoryId);
 }
