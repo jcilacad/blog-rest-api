6b085289920298d23a0395c9c55cd221a00fd454
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-25 13:45:41 +0800

    Add opendoc rest api documentation

--- src/main/java/com/ilacad/blog/blogrestapi/config/SecurityConfig.java
+++ src/main/java/com/ilacad/blog/blogrestapi/config/SecurityConfig.java
@@ -57,6 +57,8 @@ public class SecurityConfig {
                     authorize
                             .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                             .requestMatchers("/api/auth/**").permitAll()
+                            .requestMatchers("/swagger-ui/**").permitAll()
+                            .requestMatchers("/v3/api-docs/**").permitAll()
                             .anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults())
                 .exceptionHandling(exception -> exception
