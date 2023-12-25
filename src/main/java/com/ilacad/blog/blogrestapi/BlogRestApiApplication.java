2bf7d50c3d4dad72a0c0b4a77fefb936d8611b63
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-25 22:12:25 +0800

    Add rest api definition

--- src/main/java/com/ilacad/blog/blogrestapi/BlogRestApiApplication.java
+++ src/main/java/com/ilacad/blog/blogrestapi/BlogRestApiApplication.java
@@ -1,9 +1,32 @@
 package com.ilacad.blog.blogrestapi;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Blog App Rest API",
+				description = "Blog App Rest API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "John Christopher Ilacad",
+						email = "johnchristopherilacad27@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Blog App Rest Api Documentation",
+				url = "https://github.com/jcilacad/blog-rest-api"
+		)
+)
 public class BlogRestApiApplication {
 
 	public static void main(String[] args) {
