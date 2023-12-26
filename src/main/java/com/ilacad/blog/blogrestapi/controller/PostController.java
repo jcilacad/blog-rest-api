a4183a5b6628f4c4946648532a31b69de4ff6623
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-26 17:25:29 +0800

    Add a custom swagger api documentation using annotation

--- src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
@@ -4,6 +4,9 @@ import com.ilacad.blog.blogrestapi.payload.PostDto;
 import com.ilacad.blog.blogrestapi.payload.PostResponse;
 import com.ilacad.blog.blogrestapi.service.PostService;
 import com.ilacad.blog.blogrestapi.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
+@Tag(name = "POST", description = "Post Management API")
 public class PostController {
 
     private PostService postService;
@@ -22,12 +26,28 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(
+            summary = "Create a Post REST API",
+            description = "Create Post REST API is used to save post object into database."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 Created"
+    )
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Retrieve a Post REST API",
+            description = "Retrieve Post REST API is used to retrieve post object from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @GetMapping
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
@@ -37,27 +57,60 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
+    @Operation(
+            summary = "Retrieve a Post by id REST API",
+            description = "Create Post REST API is used to save post object into database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(
+            summary = "Update a Post REST API",
+            description = "Update a Post REST API is used to update post object from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") Long id) {
         PostDto postResponse = postService.updatePost(postDto, id);
 
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(
+            summary = "Delete a Post REST API",
+            description = "Delete a Post REST API is used to delete a post object from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") Long id) {
         postService.deletePostById(id);
 
         return new ResponseEntity<>("Post entity deleted successfully", HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Retrieve Posts by Category Id REST API",
+            description = "Retrieve a Posts by Category Id REST API is used " +
+                    "to retrieve list of posts object by category id."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @GetMapping("/category/{id}")
     public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable("id") Long categoryId) {
         List<PostDto> postDtos = postService.getPostsByCategory(categoryId);
