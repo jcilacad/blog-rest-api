b78a2c3608c9c270a756b595ab4c7014c5d73163
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-26 17:36:08 +0800

    Add customization of swagger documentation of comment resource

--- src/main/java/com/ilacad/blog/blogrestapi/controller/CommentController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/CommentController.java
@@ -2,6 +2,9 @@ package com.ilacad.blog.blogrestapi.controller;
 
 import com.ilacad.blog.blogrestapi.payload.CommentDto;
 import com.ilacad.blog.blogrestapi.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "COMMENT", description = "Comment Management API")
 public class CommentController {
 
     private CommentService commentService;
@@ -19,23 +23,55 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+    @Operation(
+            summary = "Create a Comment REST API",
+            description = "Create Comment REST API is used to save comment object into database."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 Created"
+    )
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDto> createComment(@PathVariable(name = "postId") Long postId,
                                                     @Valid @RequestBody CommentDto commentDto) {
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Retrieve a Comment by Post id REST API",
+            description = "Retrieve Comment by Post id REST API is used to retrieve comment object by post id from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @GetMapping("/posts/{postId}/comments")
     public List<CommentDto> getCommentsByPostId(@PathVariable(value = "postId") Long postId) {
         return commentService.getCommentsByPostId(postId);
     }
 
+    @Operation(
+            summary = "Retrieve a Comment by id REST API",
+            description = "Retrieve Comment by id REST API is used to retrieve comment object by id from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @GetMapping("/posts/{postId}/comments/{id}")
     public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") Long postId,
                                                      @PathVariable(value = "id") Long commentId) {
         return new ResponseEntity<>(commentService.getCommentById(postId, commentId), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update a Comment REST API",
+            description = "Update Comment REST API is used to update comment object by id from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @PutMapping("/posts/{postId}/comments/{id}")
     public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") Long postId,
                                                     @PathVariable(value = "id") Long commentId,
@@ -43,6 +79,14 @@ public class CommentController {
         return new ResponseEntity<>(commentService.updateComment(postId, commentId, commentDto), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete a Comment REST API",
+            description = "Delete Comment REST API is used to delete comment object by id from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @DeleteMapping("/posts/{postId}/comments/{id}")
     public ResponseEntity<String> deleteComment (@PathVariable(value = "postId") Long postId,
                                                  @PathVariable(value = "id") Long commentId) {
