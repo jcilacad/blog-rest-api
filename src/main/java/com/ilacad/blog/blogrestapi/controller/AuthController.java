e533ef62d890e28d8fcae5fbe349491851266356
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-26 17:51:00 +0800

    Add custom swagger documentation from authentication resource

--- src/main/java/com/ilacad/blog/blogrestapi/controller/AuthController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/AuthController.java
@@ -4,6 +4,9 @@ import com.ilacad.blog.blogrestapi.payload.JWTAuthResponse;
 import com.ilacad.blog.blogrestapi.payload.LoginDto;
 import com.ilacad.blog.blogrestapi.payload.RegisterDto;
 import com.ilacad.blog.blogrestapi.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "AUTHENTICATION", description = "Authentication management API")
 public class AuthController {
 
     private AuthService authService;
@@ -21,6 +25,14 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @Operation(
+            summary = "Login REST API",
+            description = "Login REST API is used to login/signin user into system."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @PostMapping(value = {"login", "signin"})
     public ResponseEntity<JWTAuthResponse> login (@RequestBody LoginDto loginDto) {
         String token = authService.login(loginDto);
@@ -31,6 +43,14 @@ public class AuthController {
         return ResponseEntity.ok(jwtAuthResponse);
     }
 
+    @Operation(
+            summary = "Register REST API",
+            description = "Register REST API is used to register/signup user into system."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @PostMapping(value = {"register", "signup"})
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
         String response = authService.register(registerDto);
