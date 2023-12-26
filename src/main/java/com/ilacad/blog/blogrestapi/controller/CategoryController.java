d091c2104737157106d4ee58e6970420a77d8ee6
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-26 17:44:45 +0800

    Add custom swagger documentation from category resource

--- src/main/java/com/ilacad/blog/blogrestapi/controller/CategoryController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/CategoryController.java
@@ -2,6 +2,9 @@ package com.ilacad.blog.blogrestapi.controller;
 
 import com.ilacad.blog.blogrestapi.payload.CategoryDto;
 import com.ilacad.blog.blogrestapi.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@Tag(name = "CATEGORY", description = "Category Management API")
 public class CategoryController {
 
     private CategoryService categoryService;
@@ -19,24 +23,56 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @Operation(
+            summary = "Create a Category REST API",
+            description = "Create Category REST API is used to create a category object into database."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 Created"
+    )
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto) {
         CategoryDto savedCategory = categoryService.addCategory(categoryDto);
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
     }
-    
+
+    @Operation(
+            summary = "Retrieve a Category by id REST API",
+            description = "Retrieve Category REST API is used to retrieve a category object from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<CategoryDto> getCategory(@PathVariable(name = "id") Long categoryId) {
         CategoryDto categoryDto = categoryService.getCategory(categoryId);
         return ResponseEntity.ok(categoryDto);
     }
 
+    @Operation(
+            summary = "Retrieve a Category REST API",
+            description = "Retrieve Category REST API is used to retrieve a list of categories object from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @GetMapping
     public ResponseEntity<List<CategoryDto>> getCategories() {
         return ResponseEntity.ok(categoryService.getCategories());
     }
 
+    @Operation(
+            summary = "Update a Category REST API",
+            description = "Update Category REST API is used to update a category object from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto,
@@ -45,6 +81,14 @@ public class CategoryController {
         return ResponseEntity.ok(updatedCategory);
     }
 
+    @Operation(
+            summary = "Delete a Category REST API",
+            description = "Delete Category REST API is used to delete a category object from database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 Success"
+    )
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCategory(@PathVariable("id") Long categoryId) {
