b3fe3edfb25780604555473f9b5f6d943125fae7
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-27 21:37:03 +0800

    Add profiles for dev and prod. Fix minor issues for retrieving all posts.

--- src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
+++ src/main/java/com/ilacad/blog/blogrestapi/controller/PostController.java
@@ -50,10 +50,10 @@ public class PostController {
     )
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIR, required = false) String sortDir) {
+            @RequestParam(name = "pageNo", required = false, defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int pageNo,
+            @RequestParam(name = "pageSize", required = false, defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int pageSize,
+            @RequestParam(name = "sortBy", required = false, defaultValue = AppConstants.DEFAULT_SORT_BY) String sortBy,
+            @RequestParam(name = "sortDir", required = false, defaultValue = AppConstants.DEFAULT_SORT_DIR) String sortDir) {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
