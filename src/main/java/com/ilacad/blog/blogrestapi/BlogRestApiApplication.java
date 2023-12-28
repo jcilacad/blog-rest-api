661f75e446b1640450d0abcd7ca51e78c438f2d7
jcilacad <johnchristopherilacad27@gmail.com>
2023-12-28 09:04:23 +0800

    Add metadata for roles

--- src/main/java/com/ilacad/blog/blogrestapi/BlogRestApiApplication.java
+++ src/main/java/com/ilacad/blog/blogrestapi/BlogRestApiApplication.java
@@ -1,10 +1,13 @@
 package com.ilacad.blog.blogrestapi;
 
+import com.ilacad.blog.blogrestapi.entity.Role;
+import com.ilacad.blog.blogrestapi.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -27,10 +30,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 				url = "https://github.com/jcilacad/blog-rest-api"
 		)
 )
-public class BlogRestApiApplication {
+public class BlogRestApiApplication implements CommandLineRunner {
+
+	private RoleRepository roleRepository;
+
+	public BlogRestApiApplication(RoleRepository roleRepository) {
+		this.roleRepository = roleRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogRestApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role admin = new Role();
+		admin.setName("ROLE_ADMIN");
+		roleRepository.save(admin);
+
+		Role user = new Role();
+		user.setName("ROLE_USER");
+		roleRepository.save(user);
+
+	}
 }
